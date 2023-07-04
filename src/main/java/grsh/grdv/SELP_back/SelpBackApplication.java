4476c16fd3cf7b23f37967b8100c22c8f1d19a2c
Grish-Gr <101037462+Grish-Gr@users.noreply.github.com>
2023-07-04 23:55:01 +0500

    init

---
+++ src/main/java/grsh/grdv/SELP_back/SelpBackApplication.java
@@ -0,0 +1,13 @@
+package grsh.grdv.SELP_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelpBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grsh/grdv/SELP_back/SelpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package grsh.grdv.SELP_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
